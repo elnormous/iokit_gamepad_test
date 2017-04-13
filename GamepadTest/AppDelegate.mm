ac41cf48c7fe92abd53290b9877f7de19cd899cd
Elviss Strazdins <elviss@elviss.lv>
2017-04-14 01:56:00 +0300

    Add button maps for Sony and Microsoft controllers

--- GamepadTest/AppDelegate.mm
+++ GamepadTest/AppDelegate.mm
@@ -19,6 +19,73 @@ IOHIDManagerRef hidManager;
 
 static void deviceInput(void* ctx, IOReturn inResult, void* inSender, IOHIDValueRef value);
 
+enum UsageID
+{
+    LEFT_THUMB_X_USAGE_ID,
+    LEFT_THUMB_Y_USAGE_ID,
+	RIGHT_THUMB_X_USAGE_ID,
+	RIGHT_THUMB_Y_USAGE_ID,
+	LEFT_TRIGGER_USAGE_ID,
+	RIGHT_TRIGGER_USAGE_ID,
+
+    DPAD_LEFT_USAGE_ID,
+	DPAD_RIGHT_USAGE_ID,
+	DPAD_DOWN_USAGE_ID,
+	DPAD_UP_USAGE_ID,
+
+	PAUSE_BUTTON_USAGE_ID,
+	A_BUTTON_USAGE_ID,
+	B_BUTTON_USAGE_ID,
+	X_BUTTON_USAGE_ID,
+	Y_BUTTON_USAGE_ID,
+	LEFT_SHOULDER_USAGE_ID,
+	RIGHT_SHOULDER_USAGE_ID,
+};
+
+uint32_t sonyUsageMap[] = {
+    kHIDUsage_GD_X, // LEFT_THUMB_X_USAGE_ID
+	kHIDUsage_GD_Y, // LEFT_THUMB_Y_USAGE_ID
+	kHIDUsage_GD_Z, // RIGHT_THUMB_X_USAGE_ID
+	kHIDUsage_GD_Rz, // RIGHT_THUMB_Y_USAGE_ID
+	kHIDUsage_GD_Rx, // LEFT_TRIGGER_USAGE_ID
+	kHIDUsage_GD_Ry, // RIGHT_TRIGGER_USAGE_ID
+
+    0x00, //DPAD_LEFT_USAGE_ID
+    0x00, //DPAD_RIGHT_USAGE_ID
+    0x00, //DPAD_DOWN_USAGE_ID
+    0x00, //DPAD_UP_USAGE_ID
+
+    0x0A, // PAUSE_BUTTON_USAGE_ID
+	0x02, // A_BUTTON_USAGE_ID
+	0x03, // B_BUTTON_USAGE_ID
+	0x01, // X_BUTTON_USAGE_ID
+	0x04, // Y_BUTTON_USAGE_ID
+	0x05, // LEFT_SHOULDER_USAGE_ID
+	0x06 // RIGHT_SHOULDER_USAGE_ID
+};
+
+uint32_t msUsageMap[] = {
+    kHIDUsage_GD_X, // LEFT_THUMB_X_USAGE_ID
+    kHIDUsage_GD_Y, // LEFT_THUMB_Y_USAGE_ID
+    kHIDUsage_GD_Rx, // RIGHT_THUMB_X_USAGE_ID
+    kHIDUsage_GD_Ry, // RIGHT_THUMB_Y_USAGE_ID
+    kHIDUsage_GD_Z, // LEFT_TRIGGER_USAGE_ID
+    kHIDUsage_GD_Rz, // RIGHT_TRIGGER_USAGE_ID
+
+    0x0E, //DPAD_LEFT_USAGE_ID
+    0x0F, //DPAD_RIGHT_USAGE_ID
+    0x0D, //DPAD_DOWN_USAGE_ID
+    0x0C, //DPAD_UP_USAGE_ID
+
+    0x09, // PAUSE_BUTTON_USAGE_ID
+    0x01, // A_BUTTON_USAGE_ID
+    0x02, // B_BUTTON_USAGE_ID
+    0x03, // X_BUTTON_USAGE_ID
+    0x04, // Y_BUTTON_USAGE_ID
+    0x05, // LEFT_SHOULDER_USAGE_ID
+    0x06 // RIGHT_SHOULDER_USAGE_ID
+};
+
 class GamepadElement
 {
 public:
@@ -34,8 +101,9 @@ public:
         element(aElement)
     {
         IOHIDElementType elementType = IOHIDElementGetType(element);
-        uint32_t usage = IOHIDElementGetUsage(element);
+        usage = IOHIDElementGetUsage(element);
         uint32_t usagePage = IOHIDElementGetUsagePage(element);
+
         min = IOHIDElementGetPhysicalMin(element);
         max = IOHIDElementGetPhysicalMax(element);
 
@@ -63,6 +131,7 @@ public:
     Type getType() const { return type; }
     CFIndex getMin() const { return min; }
     CFIndex getMax() const { return max; }
+    uint32_t getUsage() const { return usage; }
 
     float normalizeValue(CFIndex value) const
     {
@@ -74,6 +143,7 @@ protected:
     Type type = Type::NONE;
     CFIndex min = 0;
     CFIndex max = 0;
+    uint32_t usage;
 };
 
 class Gamepad
@@ -94,22 +164,44 @@ public:
             uniqueId = [currentId integerValue];
         }
 
-        NSNumber* deviceId = (NSNumber*)IOHIDDeviceGetProperty(device, CFSTR(kIOHIDPhysicalDeviceUniqueIDKey));
-        if (deviceId)
-        {
-            uniqueDeviceId = [deviceId integerValue];
-        }
-
         NSNumber* vendor = (NSNumber*)IOHIDDeviceGetProperty(device, CFSTR(kIOHIDVendorIDKey));
         if (vendor)
         {
             vendorId = [vendor integerValue];
         }
 
+        NSNumber* deviceId = (NSNumber*)IOHIDDeviceGetProperty(device, CFSTR(kIOHIDPhysicalDeviceUniqueIDKey));
+        if (deviceId)
+        {
+            uniqueDeviceId = [deviceId integerValue];
+        }
+
         NSNumber* product = (NSNumber*)IOHIDDeviceGetProperty(device, CFSTR(kIOHIDProductIDKey));
         if (product)
         {
             productId = [product integerValue];
+        }
+
+        if (vendorId == 0x054C) // Sony
+        {
+            if (productId == 0x05C4) // DualShock 4
+            {
+                usageMap = sonyUsageMap;
+            }
+        }
+        else if (vendorId == 0x045E) // Microsoft
+        {
+            if (productId == 0x028E || productId == 0x028F) // 360 wired/wireless
+            {
+                usageMap = msUsageMap;
+            }
+        }
+        else if (vendorId == 0x057E) // Nintendo
+        {
+        }
+        else
+        {
+            std::cout << "Unknown vendor: " << vendorId << std::endl;
         }
 
         CFArrayRef elementArray = IOHIDDeviceCopyMatchingElements(device, NULL, kIOHIDOptionsTypeNone);
@@ -164,6 +256,8 @@ public:
         return nullptr;
     }
 
+    uint32_t* getUsageMap() const { return usageMap; }
+
 protected:
     IOHIDDeviceRef device = Nil;
     std::map<IOHIDElementCookie, GamepadElement> elements;
@@ -172,6 +266,7 @@ protected:
     uint64_t uniqueDeviceId = 0;
     uint64_t vendorId = 0;
     uint64_t productId = 0;
+    uint32_t* usageMap = nullptr;
 };
 
 std::map<IOHIDDeviceRef, std::shared_ptr<Gamepad>> gamepads;
@@ -193,6 +288,14 @@ static void deviceInput(void* ctx, IOReturn inResult, void* inSender, IOHIDValue
 
             if (gamepadElement->getType() == GamepadElement::Type::BUTTON)
             {
+                if (gamepad->getUsageMap())
+                {
+                    if (gamepadElement->getUsage() == gamepad->getUsageMap()[A_BUTTON_USAGE_ID]) std::cout << "Button A" << std::endl;
+                    else if (gamepadElement->getUsage() == gamepad->getUsageMap()[B_BUTTON_USAGE_ID]) std::cout << "Button B" << std::endl;
+                    else if (gamepadElement->getUsage() == gamepad->getUsageMap()[X_BUTTON_USAGE_ID]) std::cout << "Button X" << std::endl;
+                    else if (gamepadElement->getUsage() == gamepad->getUsageMap()[Y_BUTTON_USAGE_ID]) std::cout << "Button Y" << std::endl;
+                }
+
                 std::cout << "Button input: " << integerValue << std::endl;
             }
             else if (gamepadElement->getType() == GamepadElement::Type::HAT)
@@ -201,7 +304,7 @@ static void deviceInput(void* ctx, IOReturn inResult, void* inSender, IOHIDValue
             }
             else if (gamepadElement->getType() == GamepadElement::Type::ANALOG)
             {
-                std::cout << "Analog value: " << gamepadElement->normalizeValue(integerValue) << std::endl;
+                //std::cout << "Analog value: " << gamepadElement->normalizeValue(integerValue) << std::endl;
             }
         }
     }
